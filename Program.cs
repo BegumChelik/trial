4297520a6b15e1b4e99f1c04b172f1276d37e89e
BegumChelik <45876232+BegumChelik@users.noreply.github.com>
2018-12-14 16:16:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using CoffeeShop.Console.Adapters;
+using CoffeeShop.Console.Entity;
+using CoffeeShop.Console.Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Customer { FirstName="Begm",SecondName="Çelik",BirthDate=new DateTime(1990,08,30),NationalId="67030150204"});
+            System.Console.ReadLine();
+        }
+    }
+}
